a4b495291fe3cad886bf7b747c3635288e70cf01
MykolaMazhula <mykm3ua@gmail.com>
2025-09-26 21:29:08 +0200

    Add register endpoint

--- users/urls.py
+++ users/urls.py
@@ -5,12 +5,16 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from books.urls import app_name
+from users.views import CreateUserView
+
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path(
-        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+        "token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
     ),
-    path(
-        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
-    ),
-    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("register", CreateUserView.as_view(), name="register"),
 ]
+
+
+app_name = "users"
