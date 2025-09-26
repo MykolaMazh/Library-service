c2d60cf1448d42ada547f5a8920f7dff887a46f8
MykolaMazhula <mykm3ua@gmail.com>
2025-09-26 12:41:51 +0200

    Add additional Author model

--- books/models.py
+++ books/models.py
@@ -2,20 +2,31 @@ from django.db import models
 from decimal import Decimal
 from django.core.validators import MinValueValidator
 
+
+class Author(models.model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=35)
+
+    def __str__(self):
+        return f"{self.last_name} {self.last_name}"
+
+
 class Book(models.Model):
     class CoverChoices(models.TextChoices):
         HARD = "HARD", "Hard"
         SOFT = "SOFT", "Soft"
 
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, related_name="written_books")
     cover = models.CharField(
-        max_length=4,
-        choices=CoverChoices.choices,
-        default=CoverChoices.HARD
+        max_length=4, choices=CoverChoices.choices, default=CoverChoices.HARD
     )
-    inventory = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)])
-    daily_fee = models.DecimalField(max_digits=4, decimal_places=2, default=Decimal("0.00"))
+    inventory = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+    daily_fee = models.DecimalField(
+        max_digits=4, decimal_places=2, default=Decimal("0.00")
+    )
 
     def __str__(self):
         return f"{self.title} by {self.author}"
