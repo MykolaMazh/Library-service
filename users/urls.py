7ddb1d19baaa97a324574de5f848e1aa224bd4a4
MykolaMazhula <mykm3ua@gmail.com>
2025-09-27 08:33:15 +0200

    Add RetrieveUpdateUserView and "me" endpoint

--- users/urls.py
+++ users/urls.py
@@ -2,18 +2,15 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView,
 )
 
-from books.urls import app_name
-from users.views import CreateUserView
+from users.views import CreateUserView, RetrieveUpdateUserView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path(
-        "token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
-    ),
-    path("register", CreateUserView.as_view(), name="register"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("me/", RetrieveUpdateUserView.as_view(), name="me"),
 ]
 
 
--- users/views.py
+++ users/views.py
@@ -1,7 +1,24 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-from users.serializers import UserCreateSerializer
+from users.serializers import UserCreateSerializer, UserUpdateSerializer
+
+User = get_user_model()
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserCreateSerializer
+
+
+class RetrieveUpdateUserView(generics.RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserUpdateSerializer
+
+    def get_object(self):
+        return User.objects.get(pk=self.request.user.pk)
+
+    # def get_serializer_class(self):
+    #     if self.request.method == "GET":
+    #         return UserRetrieveSerializer
+    #     return UserUpdateSerializer
