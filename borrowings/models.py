c28821f508ddeae1a1669f04530e6ffbd63d0e3d
MykolaMazhula <mykm3ua@gmail.com>
2025-09-27 20:18:32 +0200

    Add Borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+from books.models import Book
+
+User = get_user_model()
+
+
+class Borrowing(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True)
+
+    def clean(self):
+        if (
+            self.expected_return_date < self.borrow_date
+            or self.actual_return_date < self.borrow_date
+        ):
+            raise ValidationError(
+                "Return date cannot be earlier than borrow date."
+            )
