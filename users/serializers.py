e73233a14b9cfff3a3716ba363610bba0732ab0d
MykolaMazhula <mykm3ua@gmail.com>
2025-09-27 08:32:43 +0200

    Add UserRetrieveUpdateSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -6,7 +6,6 @@ User = get_user_model()
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = (
@@ -23,3 +22,28 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+
+class UserRetrieveUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "password",
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "is_staff",
+        )
+        extra_kwargs = {
+            "password": {"write_only": True, "min_length": 5},
+            "is_staff": {"read_only": True},
+        }
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
