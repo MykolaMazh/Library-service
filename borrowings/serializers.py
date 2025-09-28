a11ee23a8033a57377176daa78faf1e1975d12be
MykolaMazhula <mykm3ua@gmail.com>
2025-09-28 14:15:05 +0200

    Create BorrowingSerializer

---
+++ borrowings/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from borrowings.models import Borrowing
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = [
+            "book",
+            "user",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+        ]
+
+    def validate_book(self, book):
+        if book.inventory == 0:
+            raise serializers.ValidationError(
+                "This book is currently unavailable (no inventory)."
+            )
+        return book
