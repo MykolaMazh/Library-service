9c0434e5501f235a83fe90f70b4349253c89544c
MykolaMazhula <mykm3ua@gmail.com>
2025-10-02 11:40:31 +0200

    Add test_authenticate_to_borrow,c test_reduce_inventory for borrowings app

--- tests/tests.py
+++ tests/tests.py
@@ -5,30 +5,32 @@ from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
 
 from books.models import Book, Author
+from borrowings.models import Borrowing
 
 User = get_user_model()
 REGISTER_URL = "users:register"
 BOOK_LIST = "books:book-list"
 BOOK_DETAIL = "books:book-detail"
+BORROWING_LIST = "borrowings:borrowing-list"
 
 
-def create_book(_id: int):
+def create_book(id_inventory: int):
     Author.objects.create(
-        first_name=f"Author_book_{_id}_name",
-        last_name=f"Author_book_{_id}_surname",
+        first_name=f"Author_book_{id_inventory}_name",
+        last_name=f"Author_book_{id_inventory}_surname",
     )
 
     return Book.objects.create(
-        title=f"Book-{_id} title",
+        title=f"Book-{id_inventory} title",
         author=Author.objects.last(),
         cover="HARD",
-        inventory=_id,
+        inventory=id_inventory,
         daily_fee=1.15,
-        synopsis=f"Book-{_id} synopsis",
+        synopsis=f"Book-{id_inventory} synopsis",
     )
 
 
-def _create_user(self, user: str, is_staff=False):
+def _create_user(user: str, is_staff=False):
     return User.objects.create_user(
         email=f"{user}@example.com",
         password="testpass123",
@@ -53,13 +55,13 @@ class BooksApiTests(APITestCase):
             "daily_fee": 1.15,
             "synopsis": "Book synopsis",
         }
-        user = _create_user(self, "user")
+        user = _create_user("user")
         self.client.force_authenticate(user)
         response = self.client.post(reverse(BOOK_LIST), payload)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(Book.objects.count(), 0)
 
-        admin_user = _create_user(self, "admin_user", is_staff=True)
+        admin_user = _create_user("admin_user", is_staff=True)
         self.client.force_authenticate(admin_user)
         response = self.client.post(reverse(BOOK_LIST), payload)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -86,6 +88,7 @@ class BooksApiTests(APITestCase):
             msg="non-admin user should has access to get requests",
         )
 
+
 class UsersApiTests(APITestCase):
 
     def setUp(self):
@@ -104,3 +107,49 @@ class UsersApiTests(APITestCase):
         )
         user_exists = User.objects.filter(email=payload["email"]).exists()
         self.assertTrue(user_exists)
+
+
+class BorrowingApiTests(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_authenticate_to_borrow(self):
+        user1 = _create_user("user1")
+        book = create_book(1)
+        borrow_data = {
+            "book": Book.objects.last().id,
+            "expected_return_date": "2026-10-02",
+        }
+        response = self.client.post(
+            reverse(BORROWING_LIST),
+            data=borrow_data,
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        self.client.force_authenticate(user1)
+        response = self.client.post(
+            reverse(BORROWING_LIST),
+            data=borrow_data,
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(
+            Borrowing.objects.last().user,
+            user1,
+            msg="created Borrowing instance's user is active user.",
+        )
+
+    def test_reduce_inventory(self):
+        user1 = _create_user("user1")
+        book = create_book(8)
+        borrow_data = {
+            "book": Book.objects.last().id,
+            "expected_return_date": "2026-10-02",
+        }
+        self.client.force_authenticate(user1)
+        response = self.client.post(
+            reverse(BORROWING_LIST),
+            data=borrow_data,
+        )
+        book.refresh_from_db()
+        self.assertEqual(book.inventory, 7)
