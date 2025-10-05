b40b004fc9600996e2970c241f02227ada9abc03
MykolaMazhula <mykm3ua@gmail.com>
2025-10-05 11:17:45 +0200

    Add documentation for users endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from users.views import (
+    CreateUserView,
+    RetrieveUpdateUserView,
+    CustomTokenObtainPairView,
 )
 
-from users.views import CreateUserView, RetrieveUpdateUserView
-
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", CreateUserView.as_view(), name="register"),
     path("me/", RetrieveUpdateUserView.as_view(), name="me"),
