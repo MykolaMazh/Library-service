607458a29bc667e69b8e519bb4f973376ee87500
MykolaMazhula <mykm3ua@gmail.com>
2025-10-09 21:06:05 +0200

    Add send_telegram_message function

---
+++ notifications/telegram_helper.py
@@ -0,0 +1,18 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def send_telegram_message(text: str):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "Markdown"}
+    print(url, payload)
+
+    response = requests.post(url, data=payload)
+    if not response.ok:
+        raise Exception(f"Esception: {response.text}")
