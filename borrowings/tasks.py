b805868c6574e9ee34256e5d8f0f5433f106494b
MykolaMazhula <mykm3ua@gmail.com>
2025-10-12 16:12:17 +0200

    Add task get_overdue_borrowings

---
+++ borrowings/tasks.py
@@ -0,0 +1,24 @@
+from datetime import date
+from django.db.models import Q
+from celery import shared_task
+
+from borrowings.models import Borrowing
+from notifications.telegram_helper import (
+    send_telegram_overdue_message,
+    send_telegram_message,
+)
+
+
+@shared_task
+def get_overdue_borrowings():
+    today = date.today()
+    queryset = Borrowing.objects.filter(
+        Q(expected_return_date__lte=today) & Q(actual_return_date__isnull=True)
+    )
+    if queryset:
+        for borrowing in queryset:
+            send_telegram_overdue_message(borrowing)
+    else:
+        send_telegram_message("No borrowings overdue today!")
+
+    return None
