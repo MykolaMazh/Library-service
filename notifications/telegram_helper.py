ecd12c7663aa2278b05de4e3cacab7ab96b69478
MykolaMazhula <mykm3ua@gmail.com>
2025-10-12 16:11:55 +0200

    Add send_telegram_overdue_message notification

--- notifications/telegram_helper.py
+++ notifications/telegram_helper.py
@@ -2,16 +2,36 @@ import os
 import requests
 from dotenv import load_dotenv
 
+from borrowings.models import Borrowing
+
 load_dotenv()
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+
 def send_telegram_message(text: str):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "Markdown"}
 
     response = requests.post(url, data=payload)
     if not response.ok:
         raise Exception(f"Esception: {response.text}")
+
+
+def send_telegram_overdue_message(borrowing: Borrowing):
+
+    text = (
+        f"Borrowing {borrowing.id} is overdue!\n"
+        f"user: {borrowing.user}\n"
+        f"borrow date: {borrowing.borrow_date}\n"
+        "*NOT RETURNED ON*\n"
+        f"*expected return date*: {borrowing.expected_return_date}"
+    )
+    payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "Markdown"}
+
+    response = requests.post(url, data=payload)
+    if not response.ok:
+        raise Exception(f"Exception: {response.text}")
