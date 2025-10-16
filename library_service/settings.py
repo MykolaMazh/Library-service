330f82e0bfce8e61e9302942a47de430aadd07e3
MykolaMazhula <mykm3ua@gmail.com>
2025-10-16 12:38:29 +0200

    Add Stripe keys to settings.py

--- library_service/settings.py
+++ library_service/settings.py
@@ -10,10 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
 from celery.schedules import crontab
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -171,3 +175,6 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": crontab(hour=18, minute=0),
     }
 }
+
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
+STRIPE_PUBLISHABLE_KEY = os.getenv("STRIPE_PUBLISHABLE_KEY", "")
