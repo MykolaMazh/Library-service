f9dd8bf3142ce8acb108d75fc041d17ba4cf2ad9
MykolaMazhula <mykm3ua@gmail.com>
2025-10-17 14:47:52 +0200

    Add create_stripe_payment helper function

---
+++ payments/helpers.py
@@ -0,0 +1,40 @@
+import stripe
+from django.db import transaction
+
+from borrowings.models import Borrowing
+from payments.models import Payment
+
+
+def create_stripe_payment(borrowing: Borrowing, amount):
+    try:
+        with transaction.atomic():
+            checkout_session = stripe.checkout.Session.create(
+                payment_method_types=["card"],
+                mode="payment",
+                line_items=[
+                    {
+                        "price_data": {
+                            "currency": "usd",
+                            "product_data": {
+                                "name": f"Borrowing #{borrowing.id}"
+                            },
+                            "unit_amount": amount * 100,
+                        },
+                        "quantity": 1,
+                    }
+                ],
+                success_url="https://football.ua/ukraine/565295-ukrajina-z-drugoji-sprobi-obigrala-azerbajjdzhan.html",
+                cancel_url="https://football.ua/worldcup/562778-ukrajina-postupilas-franciji-na-starti-vidboru-do-chs-2026.html",
+            )
+
+            Payment.objects.create(
+                borrowing=borrowing,
+                money_to_pay=amount,
+                session_id=checkout_session.id,
+                session_url=checkout_session.url,
+                status=Payment.StatusChoices.PENDING,
+                type=Payment.TypeChoices.PAYMENT,
+            )
+
+    except Exception as e:
+        raise RuntimeError(f"Payment creation failed: {e}")
