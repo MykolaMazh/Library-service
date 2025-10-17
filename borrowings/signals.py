9f0b77745fa1275b78fe1ae59f25e07ac6b04227
MykolaMazhula <mykm3ua@gmail.com>
2025-10-17 14:50:04 +0200

    Add create_payment_session signal

--- borrowings/signals.py
+++ borrowings/signals.py
@@ -1,7 +1,10 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from django.conf import settings
+
 from borrowings.models import Borrowing
 from notifications.telegram_helper import send_telegram_message
+from payments.helpers import create_stripe_payment
 
 
 @receiver(post_save, sender=Borrowing)
@@ -15,3 +18,10 @@ def notify_new_borrowing(sender, instance, created, **kwargs):
             f"Due date: {instance.expected_return_date}"
         )
         send_telegram_message(message)
+
+
+@receiver(post_save, sender=Borrowing)
+def create_payment_session(sender, instance, created, **kwargs):
+    if created:
+        amount = instance.borrow_days * settings.DAILY_BORROW_FEE
+        create_stripe_payment(instance, amount)
