eb1170b657617e23af16b87b38166547145a90b2
MykolaMazhula <mykm3ua@gmail.com>
2025-11-10 13:10:22 +0100

    Add max_length=1000 to session_url for Payment model

--- payments/models.py
+++ payments/models.py
@@ -24,6 +24,6 @@ class Payment(models.Model):
         default=TypeChoices.PAYMENT,
     )
     borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
-    session_url = models.URLField()
+    session_url = models.URLField(max_length=1000)
     session_id = models.CharField(max_length=256)
     money_to_pay = models.DecimalField(max_digits=6, decimal_places=2)
