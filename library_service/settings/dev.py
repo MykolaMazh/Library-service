68dfbae4846c6384f86b2e9ac75d6712b9bbf27c
MykolaMazhula <mykm3ua@gmail.com>
2025-11-14 17:32:20 +0100

    Set develop database to PostgreSQL

--- library_service/settings/dev.py
+++ library_service/settings/dev.py
@@ -5,7 +5,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST_DEV"),
+        "PORT": os.getenv("POSTGRES_DB_PORT", "5432"),
     }
 }
